e70a5e278a68e8e5d89851d87a85c0b473a84950
selfassu <selfassu@gmail.com>
2020-09-17 10:39:32 +0800

    update theme

--- src/main/java/com/lqw/mprocessor/MProcessor.java
+++ src/main/java/com/lqw/mprocessor/MProcessor.java
@@ -1,12 +1,22 @@
 package com.lqw.mprocessor;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 import javax.swing.*;
 
 public class MProcessor {
 
     public static void main(String[] args){
         SwingUtilities.invokeLater(() -> {
+
+            try {
+                UIManager.setLookAndFeel(new FlatDarculaLaf());
+            } catch (UnsupportedLookAndFeelException e) {
+                e.printStackTrace();
+            }
+
             MProcessor mProcessor = new MProcessor();
+
             mProcessor.initMain();
         });
     }
